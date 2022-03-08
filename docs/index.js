c86d1baa5714254d2a3517145e717ce219016d8c
solmo <94097481+solmopark@users.noreply.github.com>
2022-03-09 03:21:37 +0900

    Add files via upload

--- docs/index.js
+++ docs/index.js
@@ -1,32 +1,9 @@
-
-!(function ($doc, $win) {
-	var screenWidth = $win.screen.width / 2,
-		screenHeight = $win.screen.height / 2,
-		$elems = $doc.getElementsByClassName("elem"),
-		validPropertyPrefix = '',
-		otherProperty = 'perspective(8000px)',
-		elemStyle = $elems[0].style;
-
-	if(typeof elemStyle.webkitTransform == 'string') {
-		validPropertyPrefix = 'webkitTransform';
-	} else if (typeof elemStyle.MozTransform == 'string') {
-		validPropertyPrefix = 'MozTransform';
-	}
-
-	$doc.addEventListener('mousemove', function (e) {
-		var centroX = e.clientX - screenWidth,
-			centroY = screenHeight - (e.clientY + 13),
-			degX = centroX * 0.04,
-			degY = centroY * 0.02,
-			$elem
-
-		for (var i = 0; i < $elems.length; i++) {
-   			$elem = $elems[i];
-			$elem.style[validPropertyPrefix] = otherProperty + 'rotateY('+ degX +'deg)  rotateX('+ degY +'deg)';
-		};
-	});
-})(document, window);
-
+/* 이미지 왜곡 */
+const tilt = $('.first-box .js-tilt').tilt({
+    maxTilt: 5,
+    perspective: 2000,
+  });
+  
 /*--------- 버블효과 ---------*/
 canvas = document.getElementsByTagName("canvas")[0];
 canvas.width = document.body.clientWidth;
